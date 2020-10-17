1a4ed16a1ba6f8a06c8b6c32e842780b871a61a9
hmdrzsharifi <hmdrzsharifi@gmail.com>
2020-10-17 18:29:53 +0330

    add PartyRole entity and repository classes

---
+++ src/main/java/com/trade/gold/domain/PartyRole.java
@@ -0,0 +1,17 @@
+package com.trade.gold.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class PartyRole {
+
+    @Id
+    private Long id;
+
+}
---
+++ src/main/java/com/trade/gold/repository/PartyRoleRepository.java
@@ -0,0 +1,8 @@
+package com.trade.gold.repository;
+
+
+import com.trade.gold.domain.PartyRole;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartyRoleRepository extends CrudRepository<PartyRole,Long> {
+}
